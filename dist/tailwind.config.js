9def39110f2cb4686d08a2092e8415b3950543d0
ksudhir <sudhir.kunnure@gmail.com>
2025-03-28 14:53:07 +0530

    new changes4

---
+++ dist/tailwind.config.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/*.css"],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('tailwindcss-all'), require('tailwindcss'), require('autoprefixer'), require('postcss')]
+};
